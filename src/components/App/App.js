ef46e09e51bacf81f5d432f71be20c33bc253543
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-03-22 19:31:57 -0600

    Organize components and add fake movie data

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import movieData from '../../movieData';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+}
+
+export default App;
