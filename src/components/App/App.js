2fcda898a72aef2200f0f459373e1a21062352ff
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-03-25 15:22:16 -0600

    feat: display fetched movie data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import movieData from '../../movieData';
 import Collection from '../Collection/Collection';
 import Nav from '../Nav/Nav';
 import MovieInfoView from '../MovieInfoView/MovieInfoView';
@@ -10,10 +9,19 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies:  movieData.movies,
+      movies: [],
       collectionView: true,
       currentMovie: null
     }
+  }
+
+  componentDidMount = () => {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then(res => res.json())
+      .then(data => {
+        // console.log('fetched data', data.movies)
+        this.setState({ movies: data.movies })
+      })
   }
 
   handleMovieView = (id) => {
@@ -21,7 +29,13 @@ class App extends Component {
       this.setState({collectionView: true})
     } else if(id){
       const movie = this.state.movies.find(movie => movie.id === +id)
-      this.setState({collectionView: false, currentMovie: movie})
+
+      fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+        .then(res => res.json())
+        .then(data => {
+          console.log('dataaaaaaaaa', data.movie)
+          this.setState({collectionView: false, currentMovie: data.movie})
+        })
     }
   }
 
--- src/components/MovieInfoView/MovieInfoView.js
+++ src/components/MovieInfoView/MovieInfoView.js
@@ -2,7 +2,7 @@ import React from "react";
 import './MovieInfoView.css'
 
 const MovieInfoView = ({movie, handleMovieView}) => {
-  const {id, poster_path, backdrop_path, title, average_rating, release_date} = movie
+  const {id, poster_path, backdrop_path, title, average_rating, release_date, tagline, runtime, overview, genres} = movie
   const backgroundStyle = {  
     backgroundImage: "url(" + backdrop_path + ")",
     height: '100vh',
@@ -17,11 +17,12 @@ const MovieInfoView = ({movie, handleMovieView}) => {
       <button id="button" onClick={(event) => handleMovieView(event.target.id)}>🍅 Go Back to Main View 🍅</button>
         <div className="movie-details">
           <h1>🍅 {title} 🍅</h1>
-          <p>tagline:</p>
+          <p>tagline: {tagline}</p>
           <p>release date: {release_date}</p>
           <p>average rating: {average_rating.toFixed(1)}</p>
-          <p>runtime:</p>
-          <p>overview:</p>
+          <p>runtime: {runtime}</p>
+          <p>overview: {overview}</p>
+          <p>genres: {genres.join(', ')}</p>
 
         </div>
       </div>
