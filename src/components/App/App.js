9bd51c3b7876b54f83b23311b4b44212ec62636d
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-03-25 14:10:51 -0500

    feat: Add functionality to main view button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,35 +16,24 @@ class App extends Component {
     }
   }
 
-  //on click of movie changes state of App, because App is what is displaying the collection. So if we want to conditionally render the movie info view (or component), it should happen in App. So we write a method in App and pass it in the props to the movie component, and when that movie component is clicked the method will run, change the state of App, and that will decide the render.  
-  //Do we need a new component? 
-
-  //id of poster/ miniMovie, then this app state can be set to the id
   handleMovieView = (id) => {
-    //set state to be the id as well
-    console.log('id', id)
-    const movie = this.state.movies.find(movie => movie.id === +id)
-    console.log(movie)
-    this.setState({collectionView: false})
-    this.setState({currentMovie: movie}, () => {
-      console.log('hotdog', this.state.currentMovie.title)
-    })
-    
+    if(id === "button"){
+      this.setState({collectionView: true})
+    } else if(id){
+      const movie = this.state.movies.find(movie => movie.id === +id)
+      this.setState({collectionView: false, currentMovie: movie})
+    }
   }
 
-  //pass in the id as a prop of Movie Info View
   render() {
     return (
       <>
         <Nav />
-          <div className='movie-container'>
-            {this.state.collectionView && <Collection movies={this.state.movies} handleMovieView={this.handleMovieView}/>}
-            {!this.state.collectionView && <MovieInfoView movie={this.state.currentMovie}/> } 
-          </div>
-    
+        <div className='movie-container'>
+          {this.state.collectionView && <Collection movies={this.state.movies} handleMovieView={this.handleMovieView}/>}
+          {!this.state.collectionView && <MovieInfoView movie={this.state.currentMovie} handleMovieView={this.handleMovieView}/> } 
+        </div>
       </>
-
-       // return instance of Collection (which has instances of Movie inside) here
     )
   }
 
--- src/components/MovieInfoView/MovieInfoView.js
+++ src/components/MovieInfoView/MovieInfoView.js
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 import './MovieInfoView'
 
 // functional component
-const MovieInfoView = ({movie}) => {
+const MovieInfoView = ({movie, handleMovieView}) => {
   const {title} = movie
+
   return (
-    <h1>{title}</h1>
+    <>
+      <button id="button" onClick={(event) => handleMovieView(event.target.id)}>Go Back to Main View</button>
+      <h1>{title}</h1>
+    </>
   )
 }
 
