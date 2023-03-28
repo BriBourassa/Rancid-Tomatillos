9f1c392dc8526f014d5ea68e4c12d63923f48c64
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-03-28 16:45:07 -0500

    test: Refactor tests with fixtures

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,53 +1,34 @@
 describe('template spec', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/');
+    cy.intercept('GET', "https://rancid-tomatillos.herokuapp.com/api/v2/movies/", {
+      statusCode: 201,
+      fixture: "movies.json"
+    })
+    .visit('http://localhost:3000/')
   });
 
-  it('Can visit the site', () => {
-    cy.visit('http://localhost:3000/')
-    .get('h1')
+  it('As a user, when I load the application, I can see the title', () => {
+    cy.get('h1')
     .contains('🍅 RANCID 🍅 TOMATILLOS 🍅')
   })
 
-  //As a user, when I load the application, I can see a collection of movies.
-  //get the movie container
-  //get div 
-  //contains 'img', 'img path'
-
   it('As a user, when I load the application, I can see a collection of movies.', () => {
-    // cy.intercept('GET', "https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
-    //   statusCode: 201,
-    //   body: {
-    //     id:436270,
-    //     poster_path: "https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",    
-    //     backdrop_path:"https://image.tmdb.org/t/p/original//bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
-    //     title: "Black Adam",
-    //     average_rating: 4,
-    //     release_date: "2022-10-19"
-    //   }
-    // })
-    cy.visit('http://localhost:3000/')
-    .get('.movie-container')
+    cy.get('.movie-container')
     .get('#436270')
-    // .contains('img', 'src="https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg"')//how do we check the actual image
+    .find("img")
   })
 
-
-  it('As a user, when I click on a movie, they are shown additional details about that movie.', () => {
-    // cy.intercept('GET', "https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
-    //   statusCode: 201,
-
-
-    // })
-    cy.visit('http://localhost:3000/')
-    .get('.movie-container')
-    .get('#934641').click()
-    .get('.movie-details')
-    .contains("🍅 The Minute You Wake Up Dead 🍅")
+  it('As a user, when I click on a movie, I am shown additional details about that movie.', () => {
+    cy.fixture("singleMovie.json")
+    .then(movie => {
+      cy.intercept('GET',"https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270", movie)
+    })
     
+    cy.get('.movie-container')
+    .get('#436270').click()
+    .get('.movie-details')
+    .contains("🍅 Black Adam 🍅")
   })
 
-  //
- 
-
+  //test for a not found page, if someone typed an incorrect id into the url
 })
